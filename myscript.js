640dde7f186ef21101c2fa0ece19b5a225a389c2
EnzoAlz <ealzamora.26@acsamman.edu.jo>
2022-11-06 12:07:36 +0200

    ughhhh

--- myscript.js
+++ myscript.js
@@ -2,25 +2,25 @@ function main()
 {
 
         
-{
-goto(getHeight())
-goto(getWidth())
-}
 
-for(var i = 0; i < 4; i++)
+for(var i = 0; i < 1; i++)
 {
     
-var height=10
-var width=25
+var height=20
+var width=10
 
 rectangle(20,10)
-forward(height)
+forward(width)
 right(90);
-forward(width)
-right(90)
 forward(height)
 right(90)
 forward(width)
+right(90)
+forward(height)
+right(180)
+forward(height)
+
+
 
 }
 
